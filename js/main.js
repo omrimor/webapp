b798204504fff9c60cf7fbf4511aacde04d9bb65
Omri Mor <omrim@perion.com>
2014-11-13 22:37:30 +0200

    major refactoring to JS, clean up the code and made it modular

--- js/main.js
+++ js/main.js
@@ -1,42 +1,38 @@
 /* globals UTILS*/
 
 window.onload = (function() {
-	var qrSettings = UTILS.qs('.js-qrTabSettings'),
-		mtfSettings = UTILS.qs('.js-mtfTabSettings'),
+	var settings = UTILS.qsa('.tab-content-settings'),
 		tabContainer = UTILS.qs('.tab-headers'),
 		allTabs = UTILS.qsa('a[role="tab"]'),
-		tabBody = UTILS.qsa('div[role="tabpanel"]'),
-		qrSettingsBtn = UTILS.qs('.js-qrBtnSettings'),
-		mtfSettingsBtn = UTILS.qs('.js-mtfBtnSettings'),
+		SettingsBtn = UTILS.qsa('.action-btn.settings'),
 		notification = UTILS.qs('.notifications'),
-		openInNewTabIcon  = UTILS.qs('.action-btn.expand'),
-		selectBoxContainer = UTILS.qs('.select--choose-iframe'),
-		selectBox = UTILS.qs('#choose-iframe-select'),
-		submitBtn = UTILS.qs('.btn.btn__submit-form'),
-		cancelFormBtn = UTILS.qs('.link.cancel-form'),
-		inputFieldsQr = UTILS.qsa('.settings-field.js-qr'),
-		inputTypeURL = UTILS.qsa('input[type="url"]'),
-		inputTypeText = UTILS.qsa('input[type="text"]'),
-		collectInputArray = [],
-		qrIframeContainer = UTILS.qs('.tab-content-body--iframe'),
-		notificationMsg = notification.childNodes[1],
-		mtfIframe = UTILS.qs('.js-mtfIframe');
+		openInNewTabIcon  = UTILS.qsa('.action-btn.expand'),
+		selectBoxContainer = UTILS.qsa('.select--choose-iframe'),
+		selectBox = UTILS.qsa('.choose-iframe-select'),
+		submitBtn = UTILS.qsa('.btn.btn__submit-form'),
+		cancelFormBtn = UTILS.qsa('.link.cancel-form'),
+		iframeContainer = UTILS.qsa('.tab-content-body--iframe');
 
-	openInNewTabIcon.classList.add('hidden');
-	notification.classList.add('hidden');
-	selectBoxContainer.classList.add('hidden');
-	qrIframeContainer.classList.add('hidden');
-
-	var getElmAttribute = function(elm){
-	    return elm.getAttribute('href').split('#')[1];
+	// Define some helper functions
+	var getElmAttribute = function(elm, attr){
+		if(attr === 'href'){
+		    return elm.getAttribute(attr).split('#')[1];
+		} else {
+			return elm.getAttribute(attr);
+		}
 	};
+
 
 	var hasClass = function (element, cls) {
 	    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 	};
 
+	for (var i = 0; i < settings.length; i++) {
+		hasClass(settings[i], 'hidden');
+	}
+
 	var getMatchingPanel = function(tab){
-		var tabAttr = getElmAttribute(tab),
+		var tabAttr = getElmAttribute(tab, 'href'),
 			matchingPanel = document.getElementById(tabAttr);
 			return matchingPanel;
 	};
@@ -60,10 +56,17 @@ window.onload = (function() {
 	  return pattern.test(urlStr);
 	};
 
-	// Initialize if class hidden exists for the qrSettings & mtfSettings toggle
-	var qrSettingsClass = hasClass(qrSettings, 'hidden'),
-	    mtfClass = hasClass(mtfSettings, 'hidden');
+	var superAddEvent = function(elm, type, handler){
+		if(elm.length !== undefined){
+			for (var i = 0; i < elm.length; i++) {
+				UTILS.addEvent(elm[i], type, handler);
+			}
+		}else {
+			UTILS.addEvent(elm, type, handler);
+		}
+	};
 
+    // Event handlers functions
 	var setTab = function(){
 		var currentHash = location.hash.replace('panel-', '');
 
@@ -73,7 +76,7 @@ window.onload = (function() {
 
 		for (var i = 0; i < allTabs.length; i++) {
 			var currentElm = allTabs[i],
-				currentElmAttr = getElmAttribute(currentElm),
+				currentElmAttr = getElmAttribute(currentElm, 'href'),
 				currentActivePanel = getMatchingPanel(currentElm);
 
 				removeClass(currentElm);
@@ -89,67 +92,90 @@ window.onload = (function() {
 	var changeHash = function(e){
 		e.preventDefault();
 		var clicked = e.target,
-			tabAttr = getElmAttribute(clicked);
-
-		location.hash = 'panel-' + tabAttr.replace('#', '');
-	};
-
-	var keypressOpenTab = function(e){
-		e.preventDefault();
-		var focused = e.target,
-			tabAttr = getElmAttribute(focused);
+			tabAttr = getElmAttribute(clicked, 'href'),
+			currentDiv = UTILS.qs('#' + tabAttr);
 
 		if (e.keyCode === 13 || e.keyCode === 32) {
 			location.hash = 'panel-' + tabAttr.replace('#', '');
 		}
+
+		location.hash = 'panel-' + tabAttr.replace('#', '');
 	};
 
 	var escToClose = function(e){
-		var target = e.target;
+		var target = e.currentTarget,
+		dataAttr = getElmAttribute(target, 'data-settings'),
+		currentSettingBtn = UTILS.qs('[data-btn="' + dataAttr + '"]'),
+		currentDiv = UTILS.qs('[data-settings="' + dataAttr + '"]');
+
 		if(e.keyCode === 27){
-			qrSettings.classList.add('hidden');
-			qrSettingsBtn.classList.remove('active');
-			qrSettingsClass = true;
-			mtfClass = true;
+			target.classList.add('hidden');
+			currentSettingBtn.classList.remove('active');
+			currentDiv.classList.remove('active');
+			hasClass(currentDiv, 'hidden');
 		}
 	};
 
 	var toggleSettings = function(e){
 		e.preventDefault();
+		var target = e.target,
+			dataAttr = getElmAttribute(target, 'data-btn'),
+			toggleDiv = UTILS.qs('[data-settings="' + dataAttr + '"]'),
+			inputFieldsQr = UTILS.qsa('.settings-field');
 
-		if (qrSettingsClass || mtfClass){
-			this.classList.add('active');
-			qrSettings.classList.remove('hidden');
-			mtfSettings.classList.remove('hidden');
-			qrSettingsClass = false;
-			mtfClass = false;
+		if (hasClass(toggleDiv, 'hidden')){
+			target.classList.add('active');
+			toggleDiv.classList.remove('hidden');
 			inputFieldsQr[0].focus();
 		} else {
-			this.classList.remove('active');
-			qrSettings.classList.add('hidden');
-			mtfSettings.classList.add('hidden');
-			qrSettingsClass = true;
-			mtfClass = true;
+			target.classList.remove('active');
+			toggleDiv.classList.add('hidden');
 		}
 	};
 
 	var closeForm = function(e){
 		e.preventDefault();
-		qrSettings.classList.add('hidden');
-		qrSettingsBtn.classList.remove('active');
-		qrSettingsClass = true;
-		mtfClass = true;
+		var target = e.target,
+			dataAttr = getElmAttribute(target, 'data-link'),
+			currentSettingBtn = UTILS.qs('[data-btn="' + dataAttr + '"]'),
+			closeDiv = UTILS.qs('[data-settings="' + dataAttr + '"]');
+
+		closeDiv.classList.add('hidden');
+		currentSettingBtn.classList.remove('active');
+		hasClass(closeDiv, 'hidden');
 	};
 
 	var saveInput = function(e){
 		e.preventDefault();
-		var isValid = true;
+
+		var isValid = true,
+			target = e.target,
+			collectInputArray = [],
+			dataAttr = getElmAttribute(target, 'data-form'),
+			currentSettingBtn = UTILS.qs('[data-btn="' + dataAttr + '"]'),
+			inputTypeText = UTILS.qsa('[data-settings="' + dataAttr + '"] input[type="text"]'),
+			inputTypeURL = UTILS.qsa('[data-settings="' + dataAttr + '"] input[type="url"]'),
+			divContainer = UTILS.qs('[data-settings="' + dataAttr + '"]'),
+			currentSelectContainer = UTILS.qs('[data-selectContainer="' + dataAttr + '"]'),
+			currentSelectElm = UTILS.qs('[data-select="' + dataAttr + '"]'),
+			currentIframeContainer = UTILS.qs('[data-iframe="' + dataAttr + '"]'),
+			currentOpenInNewTabIcon = UTILS.qs('[data-expand="' + dataAttr + '"]');
+
 		// To make sure the array is empty on every iteration
 		collectInputArray.length = 0;
-		// To make sure the select box is empty on every iteration
-		while (selectBox.firstChild) {
-		    selectBox.removeChild(selectBox.firstChild);
+
+		if(collectInputArray.length === 0){
+			collectInputArray.length = 0;
+			currentSelectContainer.classList.add('hidden');
+			currentIframeContainer.classList.add('hidden');
+			currentOpenInNewTabIcon.classList.add('hidden');
 		}
+
+		// Make sure the select box is empty on every iteration
+		while (currentSelectElm.firstChild) {
+		    currentSelectElm.removeChild(currentSelectElm.firstChild);
+		}
+
 		// Running on i will get same place for text and url inputs
 		for (var i = 0; i < inputTypeText.length; i++) {
 			var textInput = inputTypeText[i],
@@ -188,46 +214,75 @@ window.onload = (function() {
 
 				    if(isValid){
 						collectInputArray.push({name:textValue, url: urlValue});
-						qrSettings.classList.add('hidden');
-						qrSettingsBtn.classList.remove('active');
-						qrSettingsClass = true;
-						mtfClass = true;
+						divContainer.classList.add('hidden');
+						currentSettingBtn.classList.remove('active');
+						hasClass(divContainer, 'hidden');
 
 						// Create an option Node with array[i] content and append to selectBox
 						var option = document.createElement('option');
 						option.textContent = collectInputArray[i].name;
 						option.value = collectInputArray[i].url;
-						selectBox.appendChild(option);
-					    console.log(selectBox);
+						currentSelectElm.appendChild(option);
 
 						// Show the selectBox, open in new tab icon and the iframe
-						openInNewTabIcon.classList.remove('hidden');
-						selectBoxContainer.classList.remove('hidden');
-						qrIframeContainer.classList.remove('hidden');
+						currentOpenInNewTabIcon.classList.remove('hidden');
+						currentSelectContainer.classList.remove('hidden');
+						currentIframeContainer.classList.remove('hidden');
 				    }
 			    }
+			}
+			if(isValid){
+				populateIframe(dataAttr);
+			}
+	};
+
+	var populateIframe = function(context){
+		// Change the default context 'e' of the listener function
+		if(typeof context === 'object'){
+			context = getElmAttribute(context.target, 'data-select');
+		}
+
+		var currentSelectBox = UTILS.qs('[data-select = "' + context + '"]'),
+			optionsLength = currentSelectBox.options.length,
+			currentIndex = currentSelectBox.options[currentSelectBox.selectedIndex],
+			currentIframeContainer = UTILS.qs('[data-iframe="' + context + '"]'),
+		    optionVal = currentIndex.value;
+
+	    currentIframeContainer.src = optionVal;
+	};
+
+	var openInNewTab = function(e){
+		e.preventDefault();
+		var target = e.target,
+			dataAttr = getElmAttribute(target, 'data-expand'),
+			currentIframeContainer = UTILS.qs('[data-iframe="' + dataAttr + '"]'),
+			currentUrl = currentIframeContainer.src;
+
+			if(hasClass(currentIframeContainer, 'hidden')){
+				return;
+			} else {
+				window.open(currentUrl, '_blank');
+				window.focus();
 			}
 	};
 
 	setTab();
 
-	UTILS.addEvent(qrSettingsBtn, 'click', toggleSettings);
-	UTILS.addEvent(mtfSettingsBtn, 'click', toggleSettings);
-	UTILS.addEvent(tabContainer, 'click', changeHash);
-	UTILS.addEvent(tabContainer, 'keypress', keypressOpenTab);
+	UTILS.addEvent(tabContainer, 'click keypress', changeHash);
 	UTILS.addEvent(window, 'hashchange', setTab);
-	UTILS.addEvent(cancelFormBtn, 'click', closeForm);
-	UTILS.addEvent(submitBtn, 'click', saveInput);
-	UTILS.addEvent(qrSettings, 'keyup', escToClose);
+	superAddEvent(SettingsBtn, 'click', toggleSettings);
+	superAddEvent(cancelFormBtn, 'click', closeForm);
+	superAddEvent(submitBtn, 'click', saveInput);
+	superAddEvent(settings, 'keyup', escToClose);
+	superAddEvent(selectBox, 'change', populateIframe);
+	superAddEvent(openInNewTabIcon, 'click', openInNewTab);
 
 	UTILS.ajax('../webapp/data/notification.txt', {
 		done: function(response) {
+			var notificationMsg = notification.childNodes[1];
 			notification.classList.remove('hidden');
 			notificationMsg.innerHTML = response;
 			console.log(response);
-		},
-		fail: function(err){
-			// console.log(err);
 		}
 	});
 
--- js/plugins.js
+++ js/plugins.js
@@ -28,9 +28,205 @@
  * My Custom Utils library
  */
 
+/**
+ * Utils library v4
+ */
+
 var UTILS = (function () {
 
     return {
+        /**
+         * Shorthand for `querySelector`
+         *
+         * @param  {string} selector CSS like selector
+         * @return {Object}          DOM Node
+         */
+        qs: function (selector) {
+            return document.querySelector(selector);
+        },
+
+        /**
+         * Shorthand for `querySelectorAll`
+         *
+         * @param  {string} selector CSS like selector
+         * @return {Object}          DOM NodeList
+         */
+        qsa: function (selector) {
+            return document.querySelectorAll(selector);
+        },
+
+        /**
+         * Cross browser event handler
+         *
+         * @param {Object}   elm     Element on which the event will be bound
+         * @param {string}   type    Event type or types (e.g. 'click', 'click input')
+         * @param {Function} handler Callback function to run when event is fired
+         */
+        addEvent: function (elm, type, handler) {
+            var types = type.split(' '),
+                ieHandler;
+
+            // Recurse if multiple event types were given
+            if (types.length > 1) {
+                // On each iteration, remove the first value in the array
+                while (types.length) {
+                    UTILS.addEvent(elm, types.shift(), handler);
+                }
+
+                return;
+            }
+
+            // Modern browsers and IE9+
+            if (window.addEventListener) {
+                elm.addEventListener(type, handler, false);
+            // IE8
+            } else if (window.attachEvent) {
+                // Required for normalizing the "event" object,
+                // to have the same API as addEventListener
+                ieHandler = function (e) {
+                    e.target = e.target || e.srcElement;
+                    e.currentTarget = e.currentTarget || elm;
+
+                    e.stopPropagation = e.stopPropagation || function () {
+                        e.cancelBubble = true;
+                    };
+
+                    e.preventDefault = e.preventDefault || function () {
+                        e.returnValue = false;
+                    };
+
+                    return handler.call(elm, e);
+                };
+
+                // Save a reference to the handler function with a unique key
+                elm[type + handler] = ieHandler;
+
+                // Support triggering custom events
+                // Save event handlers in a special property on elm
+                elm._events = elm._events || {};
+                // Init event handlers array for the specific type, if doesn't exist
+                if (!elm._events[type]) {
+                    elm._events[type] = [handler];
+                // Add another handler, unless it already exists
+                } else if (elm._events[type].toString().indexOf(handler) < 0) {
+                    elm._events[type].push(handler);
+                }
+
+                // Bind the event, the IE way
+                elm.attachEvent('on' + type, ieHandler);
+            }
+        },
+
+        /**
+         * Check if Event constructors are supported
+         *
+         * @return {Boolean}
+         */
+        isModernEvent: function () {
+            try {
+                if (new Event('submit', {bubbles: false}).bubbles !== false) {
+                    return false;
+                } else if (new Event('submit', {bubbles: true}).bubbles !== true) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } catch (e) {
+                return false;
+            }
+        },
+
+        /**
+         * Trigger an event on a specific element
+         *
+         * Note:
+         * - bubbles/cancelable - `false` by default (not supported in IE8)
+         *
+         * @param  {Object} elm      DOM Node
+         * @param  {string} type     Event type (e.g. 'click', 'customEvent')
+         * @param  {Object} options  Options object (e.g. bubbles, cancelable)
+         */
+        emitEvent: function (elm, type, options) {
+            var evt,
+                bubbles,
+                cancelable,
+                // Will be used as a fallback event object for IE8
+                eFallback;
+
+            // Support Event options
+            options = options || {};
+            bubbles = options.bubbles !== undefined ? options.bubbles : false;
+            cancelable = options.cancelable !== undefined ? options.cancelable : false;
+
+            // Create an event object
+            // Modern browsers (except IE Any)
+            if (UTILS.isModernEvent()) {
+                evt = new Event(type, {
+                    bubbles: bubbles,
+                    cancelable: cancelable
+                });
+            // IE 9+
+            } else if (document.createEvent) {
+                evt = document.createEvent('Event');
+                evt.initEvent(type, bubbles, cancelable);
+            // IE 8
+            } else {
+                evt = document.createEventObject();
+            }
+
+            // Trigger the event
+            // Modern browsers and IE9+
+            if (elm.dispatchEvent) {
+                elm.dispatchEvent(evt);
+            // IE8
+            } else if (elm.fireEvent) {
+                // `fireEvent` will fail on non real events (i.e. custom events)
+                try {
+                    elm.fireEvent('on' + type, evt);
+                } catch (e) {
+                    // If we have any event handlers saved during `addEvent`
+                    if (elm._events && elm._events[type]) {
+                        // Support minimal Event properties
+                        eFallback = {
+                            target: elm,
+                            currentTarget: elm
+                        };
+
+                        // Trigger all event handlers for this event type
+                        for (var i = 0; i < elm._events[type].length; i++) {
+                            elm._events[type][i].call(elm, eFallback);
+                        }
+                    }
+                }
+            }
+        },
+
+        /**
+         * Cross browser event removal
+         *
+         * @param {Object}   elm     Element on which the event should be unbound
+         * @param {string}   type    Event type to unbind
+         * @param {Function} handler Reference to the original callback function
+         */
+        removeEvent: function (elm, type, handler) {
+            var handlerRef;
+
+            if (window.removeEventListener) {
+                // Modern browsers
+                elm.removeEventListener(type, handler, false);
+            } else if (window.detachEvent) {
+                // IE8 and below
+                handlerRef = elm[type + handler];
+
+                // Make sure the handler key exists
+                if (handlerRef) {
+                    elm.detachEvent('on' + type, handlerRef);
+                    // Remove the key from the object, prevent memory leaks
+                    delete elm[type + handler];
+                }
+            }
+        },
+
         /**
          * Check if a given value is a plain Object
          *
@@ -107,12 +303,6 @@ var UTILS = (function () {
                         if (options.done) {
                             options.done.call(xhr, res);
                         }
-
-                        // Trigger done callback with the proper response
-                        if (options.fail) {
-                            options.fail.call(xhr, 'Bad path, sorry');
-                        }
-
                     }
 
                 }
@@ -120,81 +310,6 @@ var UTILS = (function () {
 
             // Fire the request
             xhr.send(null);
-        },
-
-        addEvent: function (elm, type, handler) {
-            var types = type.split(' '),
-                ieHandler;
-
-            // Recurse if multiple event types were given
-            if (types.length > 1) {
-                // On each iteration, remove the first value in the array
-                while (types.length) {
-                    UTILS.addEvent(elm, types.shift(), handler);
-                }
-
-                return;
-            }
-
-            if (window.addEventListener) {
-                // Modern browsers
-                elm.addEventListener(type, handler, false);
-            } else if (window.attachEvent) {
-                // IE8 and below
-                // Required for normalizing the "event" object
-                ieHandler = function (e) {
-                    e.target = e.target || e.srcElement;
-                    e.currentTarget = e.currentTarget || elm;
-
-                    e.stopPropagation = e.stopPropagation || function () {
-                        e.cancelBubble = true;
-                    };
-
-                    e.preventDefault = e.preventDefault || function () {
-                        e.returnValue = false;
-                    };
-
-                    return handler.call(elm, e);
-                };
-
-                // Save a reference to the handler as a unique key
-                elm[type + handler] = ieHandler;
-                elm.attachEvent('on' + type, ieHandler);
-            }
-        },
-
-        /**
-         * Cross browser event removal
-         *
-         * @param {Object}   elm     Element on which the event should be unbound
-         * @param {string}   type    Event type to unbind
-         * @param {Function} handler Reference to the original callback function
-         */
-        removeEvent: function (elm, type, handler) {
-            var handlerRef;
-
-            if (window.removeEventListener) {
-                // Modern browsers
-                elm.removeEventListener(type, handler, false);
-            } else if (window.detachEvent) {
-                // IE8 and below
-                handlerRef = elm[type + handler];
-
-                // Make sure the handler key exists
-                if (handlerRef) {
-                    elm.detachEvent('on' + type, handlerRef);
-                    // Remove the key from the object, prevent memory leaks
-                    delete elm[type + handler];
-                }
-            }
-        },
-
-        qs: function(selector){
-            return document.querySelector(selector);
-        },
-
-        qsa: function(selector){
-            return document.querySelectorAll(selector);
         }
     };
 }());
